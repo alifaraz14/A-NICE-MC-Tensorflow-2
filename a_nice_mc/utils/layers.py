29a3ad5a64582f6d536a62ab162244b1067af093
Ali Faraz <ali21579@gmail.com>
2021-10-10 22:21:04 +0530

    Update layers.py

--- a_nice_mc/utils/layers.py
+++ a_nice_mc/utils/layers.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
-import tensorflow.contrib.layers as tcl
-
+#import tensorflow.contrib.layers as tcl
+import tf_slim as slim
 
 def dense(inputs, num_outputs, activation_fn=tf.identity, normalizer_fn=None, normalizer_params=None):
-    return tcl.fully_connected(inputs, num_outputs, activation_fn=activation_fn,
+    return slim.fully_connected(inputs, num_outputs, activation_fn=activation_fn,
                                normalizer_fn=normalizer_fn, normalizer_params=normalizer_params)
 
 
